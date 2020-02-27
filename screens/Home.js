08347ec1b2ed9b8fa7fd7d3700bf1e83d51e199e
unknown <imalijawad@gmail.com>
2020-02-27 14:43:25 +0500

    Added FlatList

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,72 @@
 import React, { Component } from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, FlatList } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import ImageSlider from 'react-native-image-slider';
 import MyIcon from '../components/MyIcon'
 import { Button, Card } from 'react-native-elements';
 
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Samsung LED',
+    imageSrc: require('../assets/images/tv-img.jpg'),
+    price: 1000,
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-2',
+    title: 'Samsung LED',
+    imageSrc: require('../assets/images/tv-img.jpg'),
+    price: 100,
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-3',
+    title: 'Samsung LED',
+    imageSrc: require('../assets/images/tv-img.jpg'),
+    price: 500,
+  },
+  {
+    id: '58694a0f-3da1-d-bd96-145571e29d72',
+    title: 'Samsung LED',
+    imageSrc: require('../assets/images/tv-img.jpg'),
+    price: 500,
+  },
+  {
+    id: '58694a0f-3da1-4-bd96-145571e29d72',
+    title: 'Samsung LED',
+    imageSrc: require('../assets/images/tv-img.jpg'),
+    price: 500,
+  },
+  {
+    id: '58694a0f-3da1-471f-6-145571e29d72',
+    title: 'Samsung LED',
+    imageSrc: require('../assets/images/tv-img.jpg'),
+    price: 500,
+  },
+];
+
+function Item({ title, imageSrc, price }) {
+  return (
+    <View style={{ width: 50 + "%", margin: 'auto' }}>
+      <Card
+        imageStyle={{ width: 100 + "%" }}
+        image={imageSrc}>
+        <Text style={styles.cardItmName}>{title}</Text>
+        <Text style={styles.cartItmPrice}>{price}</Text>
+        <Button
+          title='Add to '
+          iconRight={true}
+          icon={<MyIcon name='shopping-cart' color='#fff' size={20} />}
+          />
+      </Card>
+    </View>
+  );
+}
+
 export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 0.4 }}>
           <ImageSlider
             autoPlayWithInterval={3000}
             images={[
@@ -22,18 +79,14 @@ export default class Home extends Component {
               'http://placeimg.com/640/480/any',
             ]} />
         </View>
-        <View style={{ flex: 1, flexDirection:'row' }}>
-          <Card
-          containerStyle={{height: 265}}
-            image={require('../assets/images/robot-dev.png')}>
-            <Text style={{ marginBottom: 10 }}>
-              The idea with React Native Elements is more about component structure than actual design</Text>
-            <Button
-              icon={<MyIcon name='code' color='#fff' size={20} />}
-              buttonStyle={{ borderRadius: 0, margin: 0 }}
-              title='VIEW NOW' />
-          </Card>
+        <View style={{ flex: 1 }}>
+          <FlatList
+            data={DATA}
+            numColumns={2}
+            renderItem={({ item }) => <Item title={item.title} imageSrc={item.imageSrc} price={item.price} />}
+          />
         </View>
+
       </View>
     );
   }
@@ -45,4 +98,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingTop: 20,
   },
+  cardItmName: {
+    marginBottom: 10,
+    fontWeight: 'bold'
+  },
+  cardItmPrice: {
+    margin: 10,
+  }
 });
